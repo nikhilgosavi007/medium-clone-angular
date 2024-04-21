5c7b0b79e10eea09a874da7c75fd6d54d90809ae
nikhilgosavi <nikhil.gosavi@pentacor.de>
2024-04-21 11:36:44 +0200

    implement localstorage service and redirect

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -2,17 +2,23 @@ import {Actions, createEffect, ofType} from '@ngrx/effects'
 import {inject} from '@angular/core'
 import {AuthService} from '../services/auth.service'
 import {authActions} from './actions'
-import {catchError, map, of, switchMap} from 'rxjs'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
 import {CurrentUserInterface} from '../../shared/types/currentUser.interface'
 import {HttpErrorResponse} from '@angular/common/http'
+import {PersistenceService} from '../../shared/services/persistence.service'
+import {Router} from '@angular/router'
 
 export const registerEffect = createEffect(
-  (action$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    action$ = inject(Actions),
+    authService = inject(AuthService),
+    persistenceService = inject(PersistenceService)
+  ) => {
     return action$.pipe(
       ofType(authActions.register),
       switchMap((register) => {
         return authService.register(register.request).pipe(map((currentUser: CurrentUserInterface) => {
-
+            persistenceService.set('accessToken', currentUser.token)
             return authActions.registerSuccess({currentUser})
           }),
           catchError((errResponse: HttpErrorResponse) => {
@@ -22,3 +28,18 @@ export const registerEffect = createEffect(
       })
     )
   }, {functional: true})
+
+export const redirectAfterRegisterEffect = createEffect(
+  (
+    actions$ = inject(Actions),
+    router = inject(Router)
+  ) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/').catch(e => console.log('Error navigating to "/"'))
+      })
+    )
+  },
+  {functional: true, dispatch: false}
+)
